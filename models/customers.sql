88ce0d0f36258c152e5d20faa9c2268d8c8f629a
Liam Maloney <malonlia@oregonstate.edu>
2023-08-27 16:31:30 -0400

    Added missing comma

--- models/customers.sql
+++ models/customers.sql
@@ -37,7 +37,7 @@ final as (
 
     select
         customers.customer_id,
-        {{ full_name(customers.first_name, customers.last_name) }} as full_name
+        {{ full_name(customers.first_name, customers.last_name) }} as full_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
